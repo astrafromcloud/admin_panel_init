7ef66563f1abe67258352ae7a4f2cacb2c3363fd
Rome <zhailauramazan@gmail.com>
2024-09-27 11:37:39 +0500

    Second Commit

---
+++ app/Http/Requests/Purse/UpdateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Purse;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string|max:255',
+            'price' => 'required|numeric|min:0',
+            'quantity' => 'required|integer|min:0',
+            'color' => 'required|string|max:255',
+            'image_url' => 'nullable|string|max:255',
+            'material' => 'nullable|string|max:255',
+            'brand' => 'nullable|string|max:255',
+            'size' => 'nullable|numeric|max:255',
+        ];
+    }
+}
